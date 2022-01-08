378249723d5265054cd94bae2440e65d0fd54b9e
Mau Oliveira <96890754+mauooliveira@users.noreply.github.com>
2022-01-08 12:49:33 +0100

    Update mau_setNewLabel.py

--- mau_setNewLabel.py
+++ mau_setNewLabel.py
@@ -1,13 +1,14 @@
 #--------------------------------------------------
 # mau_setNewLabel.py
-# version: 1.0.0
-# last updated: 28.03.21
+# version: 1.0.1
+# last updated: 08.01.22 (DD.MM.YY)
 #--------------------------------------------------
 
 import nuke
 
 def setNewLabel():
     #CREATE LIST TO CHECK HOW MANY NODES ARE SELECTED
+    node = nuke.selectedNode()
     a = [i.name() for i in nuke.selectedNodes()]
     
     #IF MORE THAN 1 NODE IS SELECTED, RETURN NUKE MESSAGE
@@ -17,14 +18,8 @@ def setNewLabel():
     #IF ONLY ONE NODE IS SELECTED, PROCEED TO ADD NEW LABEL
     else:
         try:
-            #CREATE CUSTOM PANEL TO GET USER INPUT
-            #p = nuke.Panel('Set New Label')
-            #p.addSingleLineInput('New Label', '')
-            #p.setWidth(350)
-            #p.show()
-            #txt = p.value('New Label')
-
-            txt = nuke.getInput("Set New Label")
+            actualLabel = node.knob('label').value()
+            txt = nuke.getInput("Set New Label",actualLabel)
 
             if txt == None:
                 pass
